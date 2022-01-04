dc11e2c3322e7be7973cdfe26b14a362b356f8bd
Joao Gabriel <joaogabrielferr@gmail.com>
2022-01-04 00:36:19 -0300

    stack finished

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 
 import './App.css';
 import BinaryTree from './DS/BinaryTree';
+import Stack from './DS/Stack';
 
 function App() {
   return (
     <div className="App">
-      <BinaryTree></BinaryTree>
+      {/* <BinaryTree></BinaryTree> */}
+      <Stack></Stack>
     </div>  
   );
 }
--- src/DS/Stack.jsx
+++ src/DS/Stack.jsx
@@ -1,36 +1,300 @@
 import React from 'react'
+import { useRef,useEffect } from 'react';
 
 const Stack = () => {
 
     const svgref = useRef(); //reference to SVG element
     const svgContainer = useRef(); //reference to the div container that wraps the SVG element
 
+    const MAX_SIZE = 7;
 
-    class Stack{
+    const stack = [];
 
-        constructor()
+    let top = -1;
+    let place = 80;
+    const IDs = [];
+    let id = 1;
+    let pushButton = "";
+    let popButton = "";
+
+    const push = () =>{
+
+        let input = document.getElementById("pushInput");
+        if(input.value === "")return;
+        const value = parseInt(input.value);
+        if(top === MAX_SIZE)
         {
-            this.array = [];
+            console.log("stack overflow");
+            animateStackOverflow(value);
+            return;
+        }
+        stack.push(value);
+        IDs.push(id++);
+        top++;
+        console.log(stack);
+        animatePush(value,id - 1);
+    }
+
+    const pop = () =>{
+
+        if(top === -1)
+        {
+            console.log("empty stack");
+            animateEmpty();
+            return;
         }
 
-        
+        stack.pop();
+        const lastid = IDs.pop();
+        top--;
+        id--;
+        console.log(stack);
+        animatePop(lastid);
+
+    }
+
+    const animateEmpty = () =>{
+
+        let textoroot = document.createElementNS("http://www.w3.org/2000/svg" ,'text');
+        textoroot.setAttributeNS(null,"x",`53`);
+        textoroot.setAttributeNS(null,"y",`60`);
+        textoroot.setAttributeNS(null,"style","text-anchor:middle; fill:#254569 ;font-size:0.3vw; font-weight:bold; font-family:Poppins; dy=.3em");
+        textoroot.textContent = `Empty Stack`;
+        textoroot.setAttribute("class","texto");
+        svgref.current.appendChild(textoroot);
+
+        setTimeout(() => {
+            textoroot.remove();
+        }, 1000);
 
 
     }
+
+    const animatePush = (value,id) =>{
+
+        popButton.disabled = true;
+        pushButton.disabled = true;
+        let rect = document.createElementNS("http://www.w3.org/2000/svg", 'rect');
+        rect.setAttribute("x", "31");
+        rect.setAttribute("y", "-200");
+        rect.setAttribute("width", "43");
+        rect.setAttribute("height", "9");
+        rect.setAttribute("fill", "#254569");
+        rect.setAttribute("class", "rect");
+        rect.setAttribute("id", `${id}`);
+
+        svgref.current.append(rect);
+
+        setTimeout(() => {
+            
+            const r = document.getElementById(id);
+            r.setAttribute("y",`${place}`);
+            place -= 10;
+
+            let textoroot = document.createElementNS("http://www.w3.org/2000/svg" ,'text');
+            textoroot.setAttributeNS(null,"x",`53`);
+            textoroot.setAttributeNS(null,"y",`${place + 16}`);
+            textoroot.setAttributeNS(null,"style","text-anchor:middle; fill:#ffffff ;font-size:0.3vw; font-weight:bold; font-family:Poppins; dy=.3em");
+            textoroot.textContent = `${value}`;
+            textoroot.setAttribute("class","texto");
+            textoroot.setAttribute("id",`${id}t`);
+            svgref.current.appendChild(textoroot);
+            
+            setTimeout(() => {
+                console.log(r);
+                popButton.disabled = false;
+                pushButton.disabled = false;
+            }, 1000);
+
+        }, 50);
+
+        
+    }
+
+    const animateStackOverflow = (value) =>{
+
+        popButton.disabled = true;
+        pushButton.disabled = true;
+        let rect = document.createElementNS("http://www.w3.org/2000/svg", 'rect');
+        rect.setAttribute("x", "31");
+        rect.setAttribute("y", `-200`);
+        rect.setAttribute("width", "43");
+        rect.setAttribute("height", "9");
+        rect.setAttribute("fill", "#254569");
+        rect.setAttribute("class", "rect");
+        rect.setAttribute("id", `stackoverflowrect`);
+
+        svgref.current.append(rect);
+
+        setTimeout(() => {
+            
+            const r = document.getElementById("stackoverflowrect");
+            r.setAttribute("y",`${place}`);
+        }, 50);
+
+        setTimeout(() => {
+            
+            let textoroot = document.createElementNS("http://www.w3.org/2000/svg" ,'text');
+            textoroot.setAttributeNS(null,"x",`50`);
+            textoroot.setAttributeNS(null,"y",`50`);
+            textoroot.setAttributeNS(null,"style","text-anchor:middle; fill:#ed000c ;font-size:1vw; font-weight:bold; font-family:Poppins; dy=.3em");
+            textoroot.textContent = `STACK OVERFLOW`;
+            textoroot.setAttribute("class","texto");
+            textoroot.setAttribute("id","stackoverflow");
+            svgref.current.appendChild(textoroot);
+
+            setTimeout(() => {
+                
+                document.getElementById("stackoverflow").remove();
+                
+                const r = document.getElementById("stackoverflowrect");
+                    r.setAttribute("x",`200`);
+                
+                    setTimeout(() => {
+                
+                        document.getElementById("stackoverflowrect").remove();
+                        popButton.disabled = false;
+                    pushButton.disabled = false;
+                    }, 1000);
+                
+
+
+            }, 2500);
+
+        }, 1000);
+
+    }
+
+    const animatePop = (id) =>{
+
+        popButton.disabled = true;
+        pushButton.disabled = true;
+        const rect = document.getElementById(id);
+        const text = document.getElementById(`${id}t`);
+        rect.setAttribute('y','-200');
+        text.setAttribute('y','-200');
+        place += 10;
+        setTimeout(() => {
+            rect.remove();
+            text.remove();
+            popButton.disabled = false;
+            pushButton.disabled = false;
+
+        }, 700);
+
+    }
+
+
+     useEffect(() => {
+        
+        svgref.current.style.border = "1px solid #254569";
+        svgref.current.style.borderRadius = "0.3vw";
+        svgref.current.style.width = "70vw";
+        svgref.current.style.height = "90vh";
+        
+        let linha = document.createElementNS("http://www.w3.org/2000/svg", 'line');  
+            linha.setAttribute("x1",`30`);
+            linha.setAttribute("y1",`90`);
+            linha.setAttribute("x2",`75`);
+            linha.setAttribute("y2",`90`);
+            linha.setAttribute("class","linhaStack");
+            linha.setAttribute("id","base");
+            linha.style.stroke = "#254569";   
+            linha.style.strokeWidth = "0.03vw";
+
+            svgref.current.append(linha);
+
+            linha = document.createElementNS("http://www.w3.org/2000/svg", 'line');  
+            linha.setAttribute("x1",`30`);
+            linha.setAttribute("y1",`90`);
+            linha.setAttribute("x2",`30`);
+            linha.setAttribute("y2",`10`);
+            linha.setAttribute("class","linhaStack");
+            linha.setAttribute("id","leftline");
+            linha.style.stroke = "#254569";   
+            linha.style.strokeWidth = "0.03vw";
+
+            svgref.current.append(linha);
+
+            linha = document.createElementNS("http://www.w3.org/2000/svg", 'line');  
+            linha.setAttribute("x1",`75`);
+            linha.setAttribute("y1",`90`);
+            linha.setAttribute("x2",`75`);
+            linha.setAttribute("y2",`10`);
+            linha.setAttribute("class","linhaStack");
+            linha.setAttribute("id","rightline");
+            linha.style.stroke = "#254569";   
+            linha.style.strokeWidth = "0.03vw";
+
+            svgref.current.append(linha);
+
+            let textoroot = document.createElementNS("http://www.w3.org/2000/svg" ,'text');
+            textoroot.setAttributeNS(null,"x",`30`);
+            textoroot.setAttributeNS(null,"y",`8`);
+            textoroot.setAttributeNS(null,"style","text-anchor:middle; fill:#254569 ;font-size:0.2vw; font-weight:bold; font-family:Poppins; dy=.3em");
+            textoroot.textContent = `MAX_SIZE`;
+            textoroot.setAttribute("class","texto");
+            svgref.current.appendChild(textoroot);
+            
+            textoroot = document.createElementNS("http://www.w3.org/2000/svg" ,'text');
+            textoroot.setAttributeNS(null,"x",`53`);
+            textoroot.setAttributeNS(null,"y",`95`);
+            textoroot.setAttributeNS(null,"style","text-anchor:middle; fill:#254569 ;font-size:0.3vw; font-weight:bold; font-family:Poppins; dy=.3em");
+            textoroot.textContent = `STACK`;
+            textoroot.setAttribute("class","texto");
+            svgref.current.appendChild(textoroot);    
+            
+            //restricts input to only integers less than 9999999
+            //taken from https://stackoverflow.com/a/469362
+            function setInputFilter(textbox, inputFilter) {
+                ["input", "keydown", "keyup", "mousedown", "mouseup", "select", "contextmenu", "drop"].forEach(function(event) {
+                textbox.addEventListener(event, function() {
+                    if (inputFilter(this.value)) {
+                    this.oldValue = this.value;
+                    this.oldSelectionStart = this.selectionStart;
+                    this.oldSelectionEnd = this.selectionEnd;
+                    } else if (this.hasOwnProperty("oldValue")) {
+                    this.value = this.oldValue;
+                    this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+                    } else {
+                    this.value = "";
+                    }
+                });
+                });
+            }
+
+            setInputFilter(document.getElementById("pushInput"), function(value) {
+                return /^\d*$/.test(value) && (value === "" || ( parseInt(value) >= 0 && parseInt(value) <= 9999999)); });
+            
+            pushButton = document.getElementById("pushButton");
+            popButton = document.getElementById("popButton");
+    }, [])
 
 
     return (
         <div className='Stack'>
             
+            <div id="StackContainer">
             <div id="infostack">
-
+            <div id = "nameStack"><h3>Stack Visualization Tool</h3></div>
+                <div>
+                    <input type="text" placeholder='Add a element' id = "pushInput"/>
+                    <button id = "pushButton" onClick={push}>Push</button>
+                </div>
+                <div>
+                    <p></p>
+                    <button id = "popButton" onClick={pop}>Pop</button>
+                </div>
             </div>
             <div id="svgcontainerStack" ref={svgContainer}>
-                <svg  xmlns="http://www.w3.org/2000/svg"  id = "svg" viewBox="0 0 100 100" ref={svgref}>
+                <svg  xmlns="http://www.w3.org/2000/svg"  id = "svgStack" viewBox="0 0 100 100" ref={svgref}>
 
                 </svg>
             </div>
-
+            </div>
+            <div id="footer">
+                João Gabriel &nbsp; <a href='https://github.com/joaogabrielferr' target={"_blank"}><i className="fab fa-github"></i></a>
+                &nbsp;<a href="https://www.linkedin.com/in/joaogabrielferr/" target={"_blank"}><i className="fab fa-linkedin"></i></a>  </div>
         </div>
     )
 }
